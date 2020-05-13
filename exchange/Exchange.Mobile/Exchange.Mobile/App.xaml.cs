b8688f808a2b7e44d408b60f6f5e08cf8e5608b3
ed <djaziz12393@gmail.com>
2020-05-13 19:27:50 +0300

    init blank project

--- exchange/Exchange.Mobile/Exchange.Mobile/App.xaml.cs
+++ exchange/Exchange.Mobile/Exchange.Mobile/App.xaml.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Exchange.Mobile.Services;
-using Exchange.Mobile.Views;
 
 namespace Exchange.Mobile
 {
     public partial class App : Application
     {
-
         public App()
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
             MainPage = new MainPage();
         }
 
