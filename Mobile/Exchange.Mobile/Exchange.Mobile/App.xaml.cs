1c7cf57b5979dfb57f4324f0357967c80e395aca
ed <djaziz12393@gmail.com>
2020-05-13 19:03:44 +0300

    init mobile project

---
+++ Mobile/Exchange.Mobile/Exchange.Mobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Exchange.Mobile.Services;
+using Exchange.Mobile.Views;
+
+namespace Exchange.Mobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Mobile/Exchange.Mobile/Exchange.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Exchange.Mobile.Models;
+
+namespace Exchange.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
