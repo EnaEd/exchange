6a615e622e638f68018b6c78b56e7cef0ebfe160
ed <djaziz12393@gmail.com>
2020-06-09 18:31:03 +0300

    add discuss controller

---
+++ exchange/exchange/Controllers/DiscussOfferController.cs
@@ -0,0 +1,14 @@
+﻿using Exchange.Web.Shared.Constants;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exchange.Web.Presentation.Controllers
+{
+    [Route(Constant.Route.MAIN_API_ROUTE)]
+    public class DiscussOfferController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
