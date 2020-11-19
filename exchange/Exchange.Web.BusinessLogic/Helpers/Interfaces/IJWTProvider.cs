0fb1d50a3b3bf492818f6ff8cb98cda572162221
ed <djaziz12393@gmail.com>
2020-11-19 16:26:25 +0200

    merge

--- exchange/Exchange.Web.BusinessLogic/Helpers/Interfaces/IJWTProvider.cs
+++ exchange/Exchange.Web.BusinessLogic/Helpers/Interfaces/IJWTProvider.cs
@@ -1,14 +1,11 @@
 ﻿using Exchange.Web.BusinessLogic.Models;
 using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
 using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Exchange.Web.BusinessLogic.Helpers.Interfaces
 {
-    public interface IJWTProvider
+    public interface IJwtProvider
     {
         public SymmetricSecurityKey GetSymmetricSecurityKey();
         public Task<string> GetTokenAsync(UserModel model);
